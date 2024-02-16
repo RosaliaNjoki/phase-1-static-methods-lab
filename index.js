b24466137ff99be98d8bebe231951c76a678c5c9
RosaliaNjoki <njosali0@gmail.com>
2024-02-16 16:58:16 +0300

    half-way done

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 class Formatter {
   //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+  static sanitize (string){
+    return string.replace(/[^A-Za-z0-9-']+/g, "");
+
+  }
+  static titleize(){
+
+  }
 }
+let formatter = new Formatter("crocodile");
+formatter.capitalize;
+let formatter1 = new Formatter ("ca$@#tching cold");
